60d62c6426811934aaada47bbbaa5068b20d9176
Megan McKenna <meganemckenna1@gmail.com>
2022-01-06 12:25:15 -0600

    completed phase 2 - error handling

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const routes = require('./routes')
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
+const { ValidationError } = require('sequelize');
 
 const app = express();
 app.use(morgan('dev'));
@@ -41,7 +42,41 @@ app.use(
 app.use(routes);
 
 
+//Resource Not Found Error-Handler
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
 
+
+//Sequelize Error-Handler
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+});
+
+
+//Error Formatter Error-Handler
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack,
+    });
+});
 
 
 
