c745aece9d5f1e3e3b83cc0645f4816fc95b2c55
Megan McKenna <meganemckenna1@gmail.com>
2022-01-07 13:12:08 -0600

    started login page css - will finish later

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -3,53 +3,58 @@ import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
+import './LoginForm.css';
+
 function LoginFormPage() {
-  const dispatch = useDispatch();
-  const sessionUser = useSelector(state => state.session.user);
-  const [credential, setCredential] = useState('');
-  const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState([]);
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    const [credential, setCredential] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
 
     if (sessionUser) return (
         <Redirect to="/" />
-      );
+    );
 
-      const handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         setErrors([]);
         return dispatch(sessionActions.login({ credential, password }))
-          .catch(async (res) => {
-            const data = await res.json();
-            if (data && data.errors) setErrors(data.errors);
-          });
-      }
+            .catch(async (res) => {
+                const data = await res.json();
+                if (data && data.errors) setErrors(data.errors);
+            });
+    }
 
     return (
-        <div>
-            <h1>Login Page</h1>
+        <div className='loginForm'>
+            <h1 className='loginHeader'>Login Page</h1>
             <form onSubmit={handleSubmit}>
                 <ul>
                     {errors.map((error, idx) => <li key={idx}>{error}</li>)}
                 </ul>
-                <label>
-                    Username or Email
+                <label className='loginContainer'>
+                    {/* Welcome to Airbnb */}
                     <input
+                        className='loginField'
                         type="text"
                         onChange={(e) => setCredential(e.target.value)}
                         value={credential}
+                        placeholder='Username or Email'
                         required
                     />
                 </label>
-                <label>
-                    Password
+                <label className='loginContainer'>
                     <input
+                        className='loginField'
                         type="text"
                         onChange={(e) => setPassword(e.target.value)}
                         value={password}
+                        placeholder='Password'
                         required
                     />
                 </label>
-             <button type="submit">Log In</button>
+                <button className='loginButton' type="submit">Log In</button>
             </form>
         </div>
     );
