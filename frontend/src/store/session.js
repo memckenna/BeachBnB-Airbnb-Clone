42a665a4056320b669c3cc3b974d063aba1920e0
Megan McKenna <meganemckenna1@gmail.com>
2022-01-07 16:52:32 -0600

    completed logout thunk action

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -57,6 +57,15 @@ export const signup = (user) => async (dispatch) => {
   return response;
 };
 
+//Thunk Action Creator - DELETE Logout Action
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
 
 const initialState = { user: null };
 
