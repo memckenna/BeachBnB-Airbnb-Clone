de5b26e8f0eb0d363b956716feca840945d04d5b
Megan McKenna <meganemckenna1@gmail.com>
2022-03-25 16:21:15 -0500

    fixed splash resizing

--- frontend/src/components/HomePage/BecomeAHost.js
+++ frontend/src/components/HomePage/BecomeAHost.js
@@ -1,37 +1,26 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import './HomePage.css';
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormPage";
 
 const BecomeAHost = ({ isLoaded }) => {
-    // const sessionUser = useSelector(state => state.session.user);
+    const history = useHistory()
 
-    // let becomeAHostButton;
-    // if(sessionUser) {
-    //     becomeAHostButton = (
-    //         <NavLink to="/spot/new">
-    //                 <button className='host-button' >Become A Host</button>
-    //         </NavLink>
-    //     )
-    // } else {
-    //     becomeAHostButton = (
-    //         <>
-    //             <LoginFormModal />
-    //             <SignupFormModal />
-    //         </>
-    //     );
-    // }
+    const onClick = () => {
+        history.push("/spot/new")
+    }
+
 
     return (
         <div className='become-a-host-section'>
             <div className="hosting-section">
                 <img className="hosting-image" src="images/HomePageImages/beach-hosting.jpeg"></img>
                 <div className="hosting-text">Ready to host your beach home?</div>
-                <NavLink to="/spot/new">
-                    <button className='host-button' >Become A Host</button>
-                </NavLink>
+                {/* <NavLink to="/spot/new"> */}
+                    <button onClick={onClick} className='host-button' >Become A Host</button>
+                {/* </NavLink> */}
             </div>
         </div>
     )
--- frontend/src/components/HomePage/TripInspiration.js
+++ frontend/src/components/HomePage/TripInspiration.js
@@ -5,32 +5,37 @@ import { NavLink } from "react-router-dom";
 const TripInspiration = () => {
     return (
         <div className="trip-inspo-container">
-            <h2 className="trip-section-text">Inspiration for your next beach getaway</h2>
-            <ul className="trip-inspo-cards">
-                <NavLink className='trip-nav' to='/spots'>
-                    <li>
-                        <img src="images/HomePageImages/dreamy-beach.png" />
-                        <div id="trip-img-1" className="image-content">Dreamy Beach Getaway</div>
-                    </li>
-                </NavLink>
-                <NavLink className='trip-nav' to='/spots'>
-                    <li>
-                        <img src="images/HomePageImages/beach-mountains.png" />
-                        <div id="trip-img-2" className="image-content">Exciting Beach Getaway</div>
-                    </li>
-                </NavLink>
-                <NavLink className='trip-nav' to='/spots'>
-                    <li>
-                        <img src="images/HomePageImages/beach-water.png" />
-                        <div id="trip-img-3" className="image-content">Quite Beach Getaway</div>
-                    </li>
-                </NavLink>
-                <NavLink className='trip-nav' to='/spots'>
-                    <li>
-                        <img src="images/HomePageImages/beach-family.png" />
-                        <div id="trip-img-4" className="image-content">Family Beach Getaway</div>
-                    </li>
-                </NavLink>
+            <ul className="trip-inspo-cards-section">
+                <div>
+                    <h2 className="trip-section-text">Inspiration for your next beach getaway</h2>
+
+                </div>
+                <div className="trip-inspo-cards">
+                    <NavLink className='trip-nav' to='/spots'>
+                        <li>
+                            <img src="images/HomePageImages/dreamy-beach.png" />
+                            <div id="trip-img-1" className="image-content">Dreamy Beach Getaway</div>
+                        </li>
+                    </NavLink>
+                    <NavLink className='trip-nav' to='/spots'>
+                        <li>
+                            <img src="images/HomePageImages/beach-mountains.png" />
+                            <div id="trip-img-2" className="image-content">Exciting Beach Getaway</div>
+                        </li>
+                    </NavLink>
+                    <NavLink className='trip-nav' to='/spots'>
+                        <li>
+                            <img src="images/HomePageImages/beach-water.png" />
+                            <div id="trip-img-3" className="image-content">Quite Beach Getaway</div>
+                        </li>
+                    </NavLink>
+                    <NavLink className='trip-nav' to='/spots'>
+                        <li>
+                            <img src="images/HomePageImages/beach-family.png" />
+                            <div id="trip-img-4" className="image-content">Family Beach Getaway</div>
+                        </li>
+                    </NavLink>
+                </div>
             </ul>
         </div>
     )
